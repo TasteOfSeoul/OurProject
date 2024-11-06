85f6649fb53ba3249a85c55809b587f34df5b70c
peter shin <daehun47@gmail.com>
2024-11-06 22:46:21 +0900

    initial commit

---
+++ src/main/java/tasteone/TasteOfSeoul/TasteOfSeoulApplication.java
@@ -0,0 +1,13 @@
+package tasteone.TasteOfSeoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteOfSeoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasteOfSeoulApplication.class, args);
+	}
+
+}
