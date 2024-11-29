5e5678cdcfc4049a745626917e0675c87d04c053
peter shin <daehun47@gmail.com>
2024-11-29 14:07:14 +0900

    [update] Service 어노테이션 추가 에러 해결

--- src/main/java/tasteone/TasteOfSeoul/service/MyService.java
+++ src/main/java/tasteone/TasteOfSeoul/service/MyService.java
@@ -2,11 +2,13 @@ package tasteone.TasteOfSeoul.service;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import tasteone.TasteOfSeoul.domain.testVO;
 import tasteone.TasteOfSeoul.mapper.testMapper;
 
 import java.util.List;
 
+@Service
 public class MyService {
 
     @Autowired
